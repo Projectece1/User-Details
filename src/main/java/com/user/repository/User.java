bf4ca0204fd15e324d6ffb39fb83751d1a67ff09
Projectece1 <35722508+Projectece1@users.noreply.github.com>
2022-11-17 16:58:16 +0530

    Create User.java

---
+++ src/main/java/com/user/repository/User.java
@@ -0,0 +1,72 @@
+package java.com.user.repository;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    
+    private String userName;
+    
+    private String password;
+
+    private String image;
+
+    public User() {
+        super();
+    }
+
+    public User(Long id, String name, String userName, String password, String image) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.userName= userName;
+        this.password = password;
+        this.image = image;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+}
