2e096fd7a6b9daf9a147f57d39ccbb0eec18e769
Projectece1 <35722508+Projectece1@users.noreply.github.com>
2022-11-17 17:07:22 +0530

    Create InCorrectCredentialsException.java

---
+++ src/main/java/com/user/controller/InCorrectCredentialsException.java
@@ -0,0 +1,9 @@
+package java.com.user.controller;
+
+public class InCorrectCredentialsException extends RuntimeException {
+
+    public InCorrectCredentialsException(String exception) {
+        super(exception);
+    }
+
+}
