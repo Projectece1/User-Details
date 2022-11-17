3be7018a1421d3300a99bbf2ac5f0af212d82d9f
Projectece1 <35722508+Projectece1@users.noreply.github.com>
2022-11-17 23:08:19 +0530

    Update UserController.java

--- src/main/java/com/user/controller/UserController.java
+++ src/main/java/com/user/controller/UserController.java
@@ -22,6 +22,7 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    //Get the user details for the given userId
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable long id,@Param String userName, @Param String password) {
     Optional<User> registeredUser = userRepository.findById(id);
@@ -29,8 +30,7 @@ public class UserController {
             return ResponseEntity.notFound().build();
             
         if(!(registeredUser.getUserName().equals(user.getUserName()) && registeredUser.getPassword().equals(user.getPassword()))){
-        {
-          throw InCorrectCredentialsException("Invalid username and password..");
+            throw InCorrectCredentialsException("Invalid username and password..");
          }
 
         return user.get();
@@ -45,39 +45,55 @@ public class UserController {
             
         if(!(registeredUser.getUserName().equals(user.getUserName()) && registeredUser.getPassword().equals(user.getPassword()))){
         {
-          throw InCorrectCredentialsException("Incorrect username and password);
+          throw InCorrectCredentialsException("Incorrect username and password..");
          }
         userRepository.deleteById(id);
     }
 
+    // Register new user
     @PostMapping("/users/register")
-    public ResponseEntity<Object> registerUser(@RequestBody User user) {
-        User savedUser = userRepository.save(user);
-
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-                .buildAndExpand(savedUser.getId()).toUri();
-
-        return ResponseEntity.created(location).build();
-
+    public ResponseEntity<Object> registerNewUser(@RequestBody User user) {
+        try{
+            User savedUser = userRepository.save(user);
+        }catch(Exception e) {
+            return new ResponseEntity<>("Couldn't register the user due to internal error"), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>("Successfully registered the user"), HttpStatus.OK);
     }
 
+    //Update the user details
     @PutMapping("/users/{id}")
     public ResponseEntity<Object> updateUserDetails(@RequestBody User user, @PathVariable long id) {
 
+        //Fetching user details from the data base
         Optional<User> registeredUser = userRepository.findById(id);
         if (updatedUser.isEmpty())
             return ResponseEntity.notFound().build();
             
         if(!(registeredUser.getUserName().equals(user.getUserName()) && registeredUser.getPassword().equals(user.getPassword()))){
-        {
           throw InCorrectCredentialsException("Incorrect username and password);
          }
-        
-
         user.setId(id);
+        userRepository.save(user);
+        return return new ResponseEntity<>("Successfully updated the User details"), HttpStatus.OK);
+    }
+    //Upload User Imgae 
+    @PostMapping("/upload-image")
+    public ResponseEntity<FileResponse> uploadImage(@RequestParam("image") MultipartFile image, @RequestParam("id") Long id) {
+    try{
+        //Upload image to Imgur server
+        service.uploadImage(image, id);
+        //Fetch user
+        Optional<User> registeredUser = userRepository.findById(id);
+        if (updatedUser.isEmpty())
+            return ResponseEntity.notFound().build();
+        user.setId(id);
+        user.setImage(image);
+        userRepository.save(user);
+        }catch(Exception e){
+        return new ResponseEntity<>(new FileResponse(fileName, "Image is not uploaded due to internal error"), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+        return new ResponseEntity<>(new FileResponse(fileName, "Image successfully uploaded"), HttpStatus.OK);
 
-        userRepository.save(student);
-
-        return ResponseEntity.noContent().build();
     }
 }
