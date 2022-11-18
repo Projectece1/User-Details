d205faa26caf9eee68b9af9fc5244b9cbdb527bb
Projectece1 <35722508+Projectece1@users.noreply.github.com>
2022-11-18 09:02:48 +0530

    Update Service.java

--- src/main/java/com/user/service/Service.java
+++ src/main/java/com/user/service/Service.java
@@ -11,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class Service {
   
- private static final Logger logger = LogManager.getLogger(Service.class);
+private static final Logger logger = LogManager.getLogger(Service.class);
 @Autowired
 private RestTemplate restTemplate;
 
@@ -21,19 +21,28 @@ private String clientId;
 @Value("${imgur.uri}")
 private String imgurUri;
 
+//Upload images to Imgur
 public void uploadImage(String image, Long id){
-
+	
+//Setting the request params
 MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
 map.add("link", image);
-map.add("id", id);
-
+	
+//Setting headers for the request
 HttpHeaders headers = new HttpHeaders();
 headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-headers.set("Client-ID", clientId);
+headers.set("Authorization", "Client-ID" + clientId);
 
 HttpEntity<String> entity = new HttpEntity<String>(map,headers);
-
-ResponseEntity<User[]> responseEntity = restTemplate
+/*curl --location --request POST 'https://api.imgur.com/3/image' \
+--header 'Authorization: Client-ID {{clientId}}' \
+--form 'image="R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"'*/
+	
+try{
+	ResponseEntity<User[]> responseEntity = restTemplate
 		.postForEntity(imgurUri, HttpMethod.POST, entity, String.class);
+}catch(Exception e) {
+	logger.debug("Issue while uploading image to imgur"+ e.stackTrace());
+}
 }
 }
